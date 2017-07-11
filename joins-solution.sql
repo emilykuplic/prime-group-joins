2956fa9da63150ae6c39df71fd5222c904d0e3d2
Emily Kuplic <emilykuplic@gmail.com>
2017-07-11 15:27:59 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -5,15 +5,20 @@ ON "customers"."id" = "addresses"."customer_id";
 
 2. Get all orders and their line items.
 
-
+SELECT * FROM "orders" JOIN "line_items"
+ON "orders"."id" = "line_items"."order_id";
 
 3. Which warehouses have cheetos?
 
-
+SELECT "products"."id", "warehouse"."id", "warehouse_product"."product_id", "warehouse_product"."warehouse_id", "warehouse"."warehouse"
+FROM "products" JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id"
+JOIN "warehouse" ON "warehouse"."id" = "warehouse_product"."warehouse_id" WHERE "products"."description" ILIKE 'cheetos';
 
 4. Which warehouses have diet pepsi?
 
-
+SELECT "products"."id", "warehouse"."id", "warehouse_product"."product_id", "warehouse_product"."warehouse_id", "warehouse"."warehouse"
+FROM "products" JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id"
+JOIN "warehouse" ON "warehouse"."id" = "warehouse_product"."warehouse_id" WHERE "products"."description" ILIKE '%pep%';
 
 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
